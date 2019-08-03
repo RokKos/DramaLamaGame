467b21db93239620cb0591fb55d61c44cb351fd7
Rok Kos <rocki5555@gmail.com>
2019-08-03 14:54:14 +0200

    Working Meters and audio in
    Signed-off-by: Rok Kos <rocki5555@gmail.com>

--- Assets/Scripts/MainUiController.cs
+++ Assets/Scripts/MainUiController.cs
@@ -6,6 +6,16 @@ using UnityEngine.UI;
 public class MainUiController : MonoBehaviour
 {
     [SerializeField] Text main_text_;
+    [SerializeField] Text right_answer_text_;
+    [SerializeField] Text left_answer_text_;
+
+    [SerializeField] Image bar_truth_;
+    [SerializeField] Image bar_entertaiment_;
+    [SerializeField] Image bar_drama_;
+
+    private float bar_truth_fill_amount = 0.5f;
+    private float bar_entertainment_fill_amount = 0.5f;
+    private float bar_drama_fill_amount = 0.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,8 +29,31 @@ public class MainUiController : MonoBehaviour
         
     }
 
-    void SetupUI() {
+    public void  ChangeTruthFillAmount(float change) {
+        bar_truth_fill_amount += change;
+        bar_truth_fill_amount = Mathf.Min(1.0f, Mathf.Max(0.0f, bar_truth_fill_amount));
+    }
+
+    public void ChangeEntertainmetnFillAmount(float change)
+    {
+        bar_entertainment_fill_amount += change;
+        bar_entertainment_fill_amount = Mathf.Min(1.0f, Mathf.Max(0.0f, bar_entertainment_fill_amount));
+    }
+    public void ChangeDramaFillAmount(float change)
+    {
+        bar_drama_fill_amount += change;
+        bar_drama_fill_amount = Mathf.Min(1.0f, Mathf.Max(0.0f, bar_drama_fill_amount));
+    }
+
+
+    public void SetupUI() {
         main_text_.text = GoogleDocsUtils.GetMainText();
+        right_answer_text_.text = GoogleDocsUtils.GetRightAnswerText();
+        left_answer_text_.text = GoogleDocsUtils.GetLeftAnswerText();
+
+        bar_truth_.fillAmount = bar_truth_fill_amount;
+        bar_entertaiment_.fillAmount = bar_entertainment_fill_amount;
+        bar_drama_.fillAmount = bar_drama_fill_amount;
     }
 
 }
---
+++ Assets/Scripts/MainUiState.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainUiState : MonoBehaviour
+{
+
+    [SerializeField] MainUiController main_menu_controller_;
+    private enum Card { Left = -1, Right = 1 };
+
+    public void SelectCard(int side) {
+        float truth_meter_change = 0;
+        float entertaiment_meter_change = 0;
+        float drama_meter_change = 0;
+
+        Card side_ = (Card)side;
+        switch (side_) {
+            case Card.Left: {
+                    Debug.Log("Left");
+                    truth_meter_change  = GoogleDocsUtils.GetRightAnswerTruthMeterChange() / 10.0f;
+                    entertaiment_meter_change = GoogleDocsUtils.GetRightAnswerEntertainmentMeterChange() / 10.0f;
+                    drama_meter_change  = GoogleDocsUtils.GetRightAnswerDramaMeterChange() / 10.0f;
+
+                    break;
+
+            }
+
+            case Card.Right: {
+                    Debug.Log("Right");
+
+                    truth_meter_change = GoogleDocsUtils.GetLeftAnswerTruthMeterChange() / 10.0f;
+                    entertaiment_meter_change = GoogleDocsUtils.GetLeftAnswerEntertainmentMeterChange() / 10.0f;
+                    drama_meter_change = GoogleDocsUtils.GetLeftAnswerDramaMeterChange() / 10.0f;
+                    
+                    break;
+
+            }
+
+
+
+            default:
+                Debug.Log("Error");
+                break;
+        }
+
+        main_menu_controller_.ChangeTruthFillAmount(truth_meter_change);
+        main_menu_controller_.ChangeEntertainmetnFillAmount(entertaiment_meter_change);
+        main_menu_controller_.ChangeDramaFillAmount(drama_meter_change);
+        GoogleDocsUtils.SelectNewCard();
+        main_menu_controller_.SetupUI();
+    }
+}
