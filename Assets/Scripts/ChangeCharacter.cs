43cc2347c3328dd4225e768c6f9fa0ad90acc82c
Rok Kos <rocki5555@gmail.com>
2019-08-04 16:21:09 +0200

    Changing chararters
    Signed-off-by: Rok Kos <rocki5555@gmail.com>

--- Assets/Scripts/ChangeCharacter.cs
+++ Assets/Scripts/ChangeCharacter.cs
@@ -7,9 +7,9 @@ public class ChangeCharacter : MonoBehaviour
 {
     [SerializeField] Image charater_image_;
 
-    [SerializeField] List<Color> charater_avatars_;  // Change this to images
+    [SerializeField] List<Sprite> charater_avatars_;  // Change this to images
     public void ChangeCharacterAndDisable() {
-        charater_image_.color = charater_avatars_[Random.Range(0, charater_avatars_.Count)];
+        charater_image_.sprite = charater_avatars_[Random.Range(0, charater_avatars_.Count)];
         charater_image_.enabled = false;
     }
 
